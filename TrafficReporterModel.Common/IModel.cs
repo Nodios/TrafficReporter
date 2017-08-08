76eb428d81da4eae0d711a0da9f619804d0a7be4
ivansperanda <30797011+ivansperanda@users.noreply.github.com>
2017-08-08 15:24:39 +0200

    Add files via upload

---
+++ TrafficReporterModel.Common/IModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrafficReporterModel.Common
+{
+    public class IModel
+    {
+        Guid Id { get; set; }
+        Cause Cause { get; set; }
+        DateTime DateCreated { get; set; }
+        Boolean Active { get; set; }
+        Guid PictureId { get; set; }
+        int Rating { get; set; }
+
+    }
+}
---
+++ TrafficReporterModels/Report.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrafficReporterModel.Common;
+
+namespace TrafficReporterModels
+{
+    public class Report
+    {
+        Guid Id { get; set; }
+        Cause Cause { get; set; }
+        DateTime DateCreated { get; set; }
+        Boolean Active { get; set; }
+        Guid PictureId { get; set; }
+        int Rating { get; set; }
+    }
+}

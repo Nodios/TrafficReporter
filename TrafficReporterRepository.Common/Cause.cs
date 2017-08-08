3fea6a9ec149dd34efd7b05f9d2465a867e0b2f1
Josip Paulik <josip.paulik@outlook.com>
2017-08-08 14:12:05 +0200

    Rename Folders

--- TrafficReporterRepository.Common/Cause.cs
+++ TrafficReporterRepository.Common/Cause.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TrafficReporterRepository.Common
+﻿namespace TrafficReporterRepository.Common
 {
     public enum Cause
     {
-
+        
     }
 }

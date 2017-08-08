652a23a74ccd5a09d5cdfc680f72c08f81f0fae3
ivansperanda <30797011+ivansperanda@users.noreply.github.com>
2017-08-08 14:23:46 +0200

    Add files via upload

---
+++ Report.Repository.Common/Report.Model.Common/IReport.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Report.Model.Common
+{
+    public interface IReport
+    {
+        Guid Id { get; set; }
+        Cause Cause { get; set; }
+        DateTime DateCreated { get; set; }
+        Boolean Active { get; set; }
+        Guid PictureId { get; set; }
+        int Rating { get; set; }
+        
+        
+    }
+}

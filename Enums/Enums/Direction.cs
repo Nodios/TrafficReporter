1b224ae014c559aa655ce68e8f7dcbe96033ca88
Josip Paulik <josip.paulik@outlook.com>
2017-08-10 13:52:12 +0200

    Rename projects.

---
+++ Enums/Enums/Direction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enums
+{
+    public enum Direction
+    {
+        N=6,
+        E,
+        W,
+        S,
+        NE,
+        NW,
+        SE,
+        SW
+    }
+
+}

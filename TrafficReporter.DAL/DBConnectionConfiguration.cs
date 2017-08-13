4c5cdf30f2a77c1d41e783b4ad03e51444721322
Josip Paulik <josip.paulik@outlook.com>
2017-08-13 22:47:33 +0200

    Made some basic configurations for setting up DbConnection:                          -Created DbConnectionConfiguration class which has one method which gets connection string based on given provider                                                  -might be changed later                          -Created App.config file where we will store database related informations(inside TrafficReporter.DAL)                          -Created UnitTest project where we should test AS OFTEN AS POSSIBLE

---
+++ TrafficReporter.DAL/DBConnectionConfiguration.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace TrafficReporter.DAL
+{
+    /// <summary>
+    /// This class helps contribute to "factory" design pattern,
+    /// so our code doesn't depend on database provider.
+    /// See this link for more information.
+    /// https://docs.microsoft.com/en-us/dotnet/framework/data/adonet/factory-model-overview
+    /// </summary>
+    public class DBConnectionConfiguration
+    {
+
+        public static string GetConnectionStringByProvider(string providerName)
+        {
+            string returnValue = null;
+
+            var settings = ConfigurationManager.ConnectionStrings;
+
+            if (settings != null)
+            {
+                foreach (ConnectionStringSettings connectionSetting in settings)
+                {
+                    if (connectionSetting.ProviderName.Equals(providerName))
+                    {
+                        returnValue = connectionSetting.ConnectionString;
+                        break;
+                    }
+                }
+            }
+
+            return returnValue;
+        }
+    }
+}

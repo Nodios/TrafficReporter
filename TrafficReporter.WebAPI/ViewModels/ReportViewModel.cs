0499d932fe645d5ce2e22925c9b030cdd1df8faa
Josip Paulik <josip.paulik@outlook.com>
2017-08-18 12:06:14 +0200

    Create ReportViewModel.

---
+++ TrafficReporter.WebAPI/ViewModels/ReportViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TrafficReporter.Common.Enums;
+
+namespace TrafficReporter.WebAPI.ViewModels
+{
+    public class ReportViewModel
+    {
+        public Guid Id { get; set; }
+        public Cause Cause { get; set; }
+        public Direction Direction { get; set; }
+        public double Longitude { get; set; }
+        public double Lattitude { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}

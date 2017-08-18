69b4492f940b81f01721f976230b7f1e98596f68
Josip Paulik <josip.paulik@outlook.com>
2017-08-18 13:43:31 +0200

    Created form using ReportViewModel. Now we need to initalize the mappings.

--- TrafficReporter.WebAPI/ViewModels/ReportViewModel.cs
+++ TrafficReporter.WebAPI/ViewModels/ReportViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 using TrafficReporter.Common.Enums;
@@ -9,10 +10,17 @@ namespace TrafficReporter.WebAPI.ViewModels
     public class ReportViewModel
     {
         public Guid Id { get; set; }
+        [Required]
         public Cause Cause { get; set; }
+
+        [Required]
         public Direction Direction { get; set; }
+
+        [Required]
         public double Longitude { get; set; }
+
+        [Required]
         public double Lattitude { get; set; }
-        public DateTime DateCreated { get; set; }
+
     }
 }

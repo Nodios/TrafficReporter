ad987ff4e2217fc8eaa42f988d996cbffae17cb6
Josip Paulik <josip.paulik@outlook.com>
2017-08-25 13:58:42 +0200

    Get time from another table which will be used to reset time when similar report is added to database.

---
+++ get_time_by_cause.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR REPLACE FUNCTION get_time(int)
+RETURNS time
+AS
+	'SELECT time_remaining FROM cause_time WHERE id = $1'
+LANGUAGE SQL IMMUTABLE
+
+UPDATE trafreport
+SET time_remaining = get_time(cause);
+
+SELECT * FROM trafreport;
+
+
+SELECT * FROM cause_time;

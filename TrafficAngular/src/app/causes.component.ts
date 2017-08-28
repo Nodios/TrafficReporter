261d01b0b5f0ae5f8718da3403e6276ab2afe000
Mario Marko Mikić <mariomarko2@live.com>
2017-08-28 11:52:31 +0200

    Changing causes to match backend.

--- TrafficAngular/src/app/causes.component.ts
+++ TrafficAngular/src/app/causes.component.ts
@@ -6,7 +6,7 @@ import { ReportService } from './report.service';
 import {CommunicationService } from './communication.service';
 
 
-const CAUSES: number[] = [1,2,3,4,5];
+const CAUSES: number[] = [1,2,4,8,16];
 
 @Component({
   selector: 'causes',
@@ -24,6 +24,7 @@ constructor(
     ) {}
 
 onSelect(cause: number){
+  console.log(cause);
     this.currentCause = cause;
  navigator.geolocation.getCurrentPosition(this.postProblem.bind(this));
 }

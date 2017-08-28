71f1ddc5c4bcd96db80a9882d9e56f0b83acad0e
Mario Marko Mikić <mariomarko2@live.com>
2017-08-28 14:46:56 +0200

    ..

--- TrafficAngular/src/app/causes.component.ts
+++ TrafficAngular/src/app/causes.component.ts
@@ -37,7 +37,8 @@ onSelect(cause: number){
    report.Longitude=position.coords.longitude;
    report.DateCreated= new Date().toUTCString();
    //console.log(report);
-    this.problemService.createReport(report);
+    this.problemService.createReport(report)
+    .then(data =>{ console.log(data);});
     this.communicationService.activate(true);
   }
 }
--- TrafficAngular/src/app/marker.ts
+++ TrafficAngular/src/app/marker.ts
@@ -22,7 +22,7 @@ static Collection = [];
     // Add a new marker to map (as described in report)
     create(map:any, report: Report){
     let pos = {lat: report.Lattitude,lng:report.Longitude};
-    console.log(Math.log2(report.Cause));
+    console.log('Cause:'+Math.log2(report.Cause));console.log('Rating:'+report.Rating);
     let image = {url: Markers.Causes[Math.log2(report.Cause)], scaledSize: new google.maps.Size(40,40)}
         Markers.Collection[Markers.Collection.length] = new google.maps.Marker({
           position: pos,
